4e17234dbacb20bcff7c76a86d13db1c9be929e0
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-19 15:37:07 +0200

    aggiunta file

---
+++ script.js
@@ -0,0 +1,18 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Have you ever Praise the SUN?"
+  }
+})
+
+var app = new Vue({
+    el: "#image",
+    data: {
+        Url: "https://pbs.twimg.com/media/C456oBAWMAInRkH?format=jpg&name=large"
+    }
+})
